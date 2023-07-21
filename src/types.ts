8e3ae47e9095b1c1ef612dcfa96ae72c9996a9e6
Israel Casillas Jr <ICJR@users.noreply.github.com>
2023-07-20 22:52:51 -0700

    fix: type

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
-type Next<I, R> = (input: I) => R extends Promise<unknown> ? R : Promise<R> | R;
+type Next<I, R> = (input: I) => R;
 
-type Stage<I, R> = (input: I, next: Next<I, R>) => R extends Promise<unknown> ? R : Promise<R> | R;
+type Stage<I, R> = (input: I, next: Next<I, R>) => R;
 
 
 export { Next, Stage };
