ed5922ae1734bff0452f99480046523fc675e2c2
Israel Casillas Jr <ICJR@users.noreply.github.com>
2024-06-18 21:27:35 -0700

    refactor: build stage functions upfront

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,16 @@ function error(): never {
     throw new Error('Pipeline: final stage did not return a value');
 }
 
-function next<I, R>(i: number, stages: Stage<I, R>[]): Next<I, R> {
-    let n = i + 1;
 
-    return (input) => stages[i](
-        input,
-        n < stages.length ? next(n, stages) : error
-    );
-}
+export default <I, R>(...fns: Stage<I, R>[]) => {
+    let stages: Next<I, R>[] = [];
 
+    for (let i = 0, n = fns.length; i < n; i++) {
+        stages.push(
+            (input) => fns[i](input, stages[i + 1] || error)
+        );
+    }
 
-export default <I, R>(...stages: Stage<I, R>[]) => {
-    return next(0, stages);
+    return stages[0];
 };
 export { Stage, Next };
